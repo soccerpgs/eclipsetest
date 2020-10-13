89c821d5a34ec89393c5f96373092d27cdaa242e
pgs <soccerpgsfree@gmail.com>
2020-10-13 13:30:38 +0900

    111

--- coffeeonly/src/coffeeonly/Hello.java
+++ coffeeonly/src/coffeeonly/Hello.java
@@ -4,11 +4,8 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Helloaaa");
-		System.out.println("Hellobbb");
-		System.out.println("Helloccc");
-		System.out.println("Helloddd");
-		System.out.println("Helloeee");
+		System.out.println("Hello111");
+		
 	}
 
 }
